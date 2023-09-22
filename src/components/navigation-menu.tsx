53ac1b30e7160f2cb2e5fc65f1f98e5dd4ec4fb5
Lucas Luis Borges <lucasluisborges@outlook.com>
2023-09-22 11:53:11 -0300

    create navigation and account input

--- src/components/navigation-menu.tsx
+++ src/components/navigation-menu.tsx
@@ -1,84 +1,48 @@
-import {
-  CalendarIcon,
-  ClapperboardIcon,
-  Crown,
-  PlusCircleIcon,
-  PopcornIcon,
-  Settings,
-  User
-} from "lucide-react";
+'use client'
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
-import { Genre } from '@/data/movie';
+  NavigationMenu as MenuNavigation,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger
+} from "@/components/ui/navigation-menu"
+import { Genre } from '@/data/movie'
 
 export function NavigationMenu() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        Navegar
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Navegação</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>
-              <ClapperboardIcon className="mr-2 h-4 w-4" />
-              <span>Generos</span>
-            </DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
+    <MenuNavigation>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Navegação</NavigationMenuTrigger>
+          <NavigationMenuContent className='py-2'>
+            <section className='flex min-w-max'>
+              <div className='flex flex-col gap-4'>
+                <h1 className='col-span-3 px-4 py-2 uppercase opacity-75'>Navegação</h1>
+                <NavigationMenuLink href=''>
+                  <h1 className='w-60 px-4 hover:bg-foreground py-2'>Populares</h1>
+                </NavigationMenuLink>
+                <NavigationMenuLink href=''>
+                  <h1 className='w-60 px-4 hover:bg-foreground py-2'>Novidades</h1>
+                </NavigationMenuLink>
+                <NavigationMenuLink href=''>
+                  <h1 className='w-60 px-4 hover:bg-foreground py-2'>Calendário</h1>
+                </NavigationMenuLink>
+              </div>
+              <div className='min-h-full w-[2px] bg-foreground' />
+              <div className='grid grid-cols-3 gap-4'>
+                <h1 className='col-span-3 px-4 py-2 uppercase opacity-75'>Gêneros</h1>
                 {Genre.map(itens => (
-                  <DropdownMenuItem key={itens.id}>
-                    <itens.icon className="mr-2 h-4 w-4" />
-                    <span>{itens.name}</span>
-                  </DropdownMenuItem>
+                  <NavigationMenuLink key={itens.id} href={`genero/${itens.href}`}>
+                    <h1 className='w-40 px-4 hover:bg-foreground py-2'>{itens.name}</h1>
+                  </NavigationMenuLink>
                 ))}
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <PlusCircleIcon className="mr-2 h-4 w-4" />
-                  <span>Todos...</span>
-                </DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Calendário</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <PopcornIcon className="mr-2 h-4 w-4" />
-            <span>Populares</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Perfil</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Crown className="mr-2 h-4 w-4" />
-            <span>Plus</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+              </div>
+            </section>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </MenuNavigation>
   )
 }
