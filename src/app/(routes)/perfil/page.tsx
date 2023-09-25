87cca46dd9daade4174a250c0b1fe84aa8422e20
Lucas Luis Borges <lucasluisborges@outlook.com>
2023-09-25 12:04:54 -0300

    new profile page

--- src/app/(routes)/perfil/page.tsx
+++ src/app/(routes)/perfil/page.tsx
@@ -1,5 +1,30 @@
+import { SettingsIcon } from 'lucide-react';
+
+import { Avatar } from '@/components/avatar';
+import { ListMoviesCard } from '@/components/cards/list-movies-card';
+import { NewMovies } from '@/data/movie';
+
 export default function Perfil() {
   return (
-    <h1>Perfil</h1>
+    <div className='w-full flex flex-col gap-20 items-center p-2 text-white pt-20'>
+      <section className='relative w-full flex flex-col gap-4 items-center max-w-xl p-2 bg-foreground rounded-xl'>
+        <h1 className='text-2xl font-bold'>Meu perfil</h1>
+        <Avatar src='/anime.jpg' name='LB' className='h-40 w-40 cursor-pointer' />
+        <h1 className='text-lg font-medium'>Meu nome</h1>
+        <SettingsIcon className='cursor-pointer absolute right-2 top-2 text-white h-6 w-6' />
+      </section>
+      <section className='max-w-7xl p-2 w-full space-y-4 bg-foreground rounded-xl'>
+        <h1 className='text-2xl font-bold'>Meus favoritos</h1>
+        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+          {NewMovies.map(movie => (
+            <ListMoviesCard
+              key={movie.id}
+              name={movie.name}
+              episodes={movie.episodes}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
--- src/components/search-dialog.tsx
+++ src/components/search-dialog.tsx
@@ -1,36 +1,44 @@
-'use client'
+"use client";
 
-import { DialogProps } from '@radix-ui/react-dialog';
-import { Clapperboard } from 'lucide-react';
-import { useEffect, useState } from 'react';
-import { Button } from './ui/button';
-import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './ui/command';
+import { DialogProps } from "@radix-ui/react-dialog";
+import { Clapperboard } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { Button } from "./ui/button";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
 
 export function SearchDialog({ ...props }: DialogProps) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault()
-        setOpen((open) => !open)
+        e.preventDefault();
+        setOpen((open) => !open);
       }
-    }
+    };
 
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
 
   return (
     <div>
       <Button
         variant="outline"
-        className='relative w-[150px] sm:w-[200px] md:w-[300px] border-zinc-700 justify-between text-white/75'
+        className="relative w-[150px] sm:w-[200px] md:w-[300px] border-zinc-700 justify-between text-white/75"
         onClick={() => setOpen(true)}
         {...props}
       >
-        <span className='hidden md:inline-flex'>Buscar anime...</span>
-        <span className='inline-flex md:hidden'>Buscar...</span>
+        <span className="hidden md:inline-flex">Buscar anime...</span>
+        <span className="inline-flex md:hidden">Buscar...</span>
         <kbd className="pointer-events-none select-none bg-zinc-700 items-center rounded px-1.5">
           <span className="text-xs">⌘</span>K
         </kbd>
@@ -40,34 +48,35 @@ export function SearchDialog({ ...props }: DialogProps) {
         <CommandList>
           <CommandEmpty>Nenhum resultado encontrado.</CommandEmpty>
           <CommandGroup heading="Animes">
-            <CommandItem className='flex gap-2'>
+            <CommandItem className="flex gap-2">
               <Clapperboard size={14} />
               Slime
             </CommandItem>
-            <CommandItem className='flex gap-2'>
+            <CommandItem className="flex gap-2">
               <Clapperboard size={14} />
               Naruto
             </CommandItem>
-            <CommandItem className='flex gap-2'>
+            <CommandItem className="flex gap-2">
               <Clapperboard size={14} />
               Mushoku
             </CommandItem>
           </CommandGroup>
           <CommandGroup heading="Filmes">
-            <CommandItem className='flex gap-2'>
+            <CommandItem className="flex gap-2">
               <Clapperboard size={14} />
               Slime film
             </CommandItem>
-            <CommandItem className='flex gap-2'>
+            <CommandItem className="flex gap-2">
               <Clapperboard size={14} />
               Naruto film
             </CommandItem>
-            <CommandItem className='flex gap-2'>
+            <CommandItem className="flex gap-2">
               <Clapperboard size={14} />
               Mushoku film
             </CommandItem>
           </CommandGroup>
         </CommandList>
-      </CommandDialog></div>
-  )
+      </CommandDialog>
+    </div>
+  );
 }
